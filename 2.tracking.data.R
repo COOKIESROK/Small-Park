0ecff5590010df65567421074f85e75710cbdd33
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-08 08:54:05 -0800

    Update 2.tracking.data.R

--- 2.tracking.data.R
+++ 2.tracking.data.R
@@ -3,24 +3,6 @@
 ## brief description what the script file is about
 #
 
-# === 1) ride assigner =========================================================
-# for random ride picking
-random_ride <- as.data.frame(sample(x = rides,
-                                    size = park_capacity,
-                                    replace = TRUE))
-colnames(random_ride) <- "ride"
-
-assigned_ride <- random_ride
-
-### I will start by working with random but eventually switch to biased rides
-### so comment out a chunk of this section to use one or the other
-# # for biased ride picking
-# biased_ride <- sample(x = rides,
-#                          size = park_capacity,
-#                          replace = TRUE,
-#                          prob = c(0.6, 0.4, 0.1))
-# assigned_ride <- biased_ride
-
 # === 1) visitor tracker =======================================================
 # creating a list of times tamps that repeats each times tamp for each guest
 repeated_stamps <- rep(time_stamps, park_capacity)
@@ -35,10 +17,6 @@ long_visitor_tracker$visitors <- rep(visitors)
 # === 2) first time stamp tracker ==============================================
 # for this section to work properly time stamp must be 1
 long_visitor_tracker$ride <- assigned_ride
-
-# this works now we need to tidy it so the rides are columns and presence is 1
-visitor_tracker <- pivot_wider(long_visitor_tracker, names_from = "ride", 
-                               values_from = "visitors")
 
 # === 3) ..... =================================================================
 
