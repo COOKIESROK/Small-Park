0d33347cd8cca1c8a71ef2d05aeaa285313a2b7d
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-09 14:11:06 -0800

    Update 5.figures.R

--- 5.figures.R
+++ 5.figures.R
@@ -4,8 +4,14 @@
 # - change color of barplot
 # - save figures as pdf
 
-# === 1) visitor distribution time stamp 1 =====================================
-# bar plot of visitor distribution at time stamp 1
-visitor_dist_timestamp_1 <- table(visitor_tracker$ride[1:park_capacity,])
+# === 1) visitor distribution time step 1 ======================================
+# bar plot of visitor distribution at time step 1
+visitor_dist_timestep_1 <- table(visitor_tracker$ride[1:park_capacity,])
 barplot(visitor_dist_timestamp_1, ylab = "Number of visitors", 
-        xlab = "Ride", main = "Visitor distribution time stamp 1")
+        xlab = "Ride", main = "Visitor distribution time step 1")
+
+# === 2) visitor distribution time step 2 ======================================
+# bar plot of visitor distribution at time step 1
+visitor_dist_timestep_2 <- table(visitor_tracker_merged$ride[1:park_capacity,])
+barplot(visitor_dist_timestep_2, ylab = "Number of visitors", 
+        xlab = "Ride", main = "Visitor distribution time step 2")
