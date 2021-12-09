aac238dbe441f9d9292e3e48186e76852ec5400c
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-09 15:52:16 -0800

    Update 3.tracking.data.R

--- 3.tracking.data.R
+++ 3.tracking.data.R
@@ -4,12 +4,12 @@
 #
 
 # === 1) visitor tracker ts 1===================================================
-# creating a list of time stamps that repeats each times tamp for each guest
-repeated_stamps <- rep(time_stamps[1], park_capacity)
+# creating a list of time steps that repeats each times tamp for each guest
+repeated_steps <- rep(time_steps[1], park_capacity)
 
-# creating data frame with the time stamps sorted in ascending order
-visitor_tracker <- as.data.frame(matrix(sort(repeated_stamps)))
-colnames(visitor_tracker) <- c("time stamp")
+# creating data frame with the time steps sorted in ascending order
+visitor_tracker <- as.data.frame(matrix(sort(repeated_steps)))
+colnames(visitor_tracker) <- c("time step")
 
 # adding visitor column to data frame
 visitor_tracker$visitors <- rep(visitors)
@@ -18,29 +18,28 @@ visitor_tracker$visitors <- rep(visitors)
 visitor_tracker$ride <- f.ride_picker(visitor_col, assigned_ride)
 
 # adding spot in line column to the data frame
-#visitor_tracker$spot_in_line <- NA
-#visitor_tracker$spot_in_line <- f.get_spot_in_line(visitor_tracker)
 visitor_tracker$spot_in_line <- f.get_spot_in_line_looped(visitor_tracker)
 
-
 # adding status column to the data frame
-# visitor_tracker$status <- NA
-# visitor_tracker$status <- f.get_status(visitor_tracker)
 visitor_tracker$status <- f.get_status_looped(visitor_tracker)
 
 # === 2) visitor tracker ts 2===================================================
 # copying visitor tracker
 visitor_tracker2 <-data.frame(visitor_tracker)
 
-# change the time stamps
-repeated_stamps2 <- rep(time_stamps[2], park_capacity)
-visitor_tracker2[1] <- repeated_stamps2
+# change the time steps
+repeated_steps2 <- rep(time_steps[2], park_capacity)
+visitor_tracker2[1] <- repeated_steps2
 
-# assign new rides to the visitors that went on a ride in the last time step
+# done in 7 steps
+# 1. create riding_visitors data frame
+# 2. assign riding_visitors a new ride
+# 3. create waiting visitors data frame
+# 4. move waiting visitors one step up in line
+# 5. merge both data frames
+# 6. update everyones spot in line
+# 7. update everyones status
 
-
-# move waiting visitors one spot up in line
-f.move_up(visitor_tracker2)
 
 
 
