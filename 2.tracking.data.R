c78b011e751089eea51740e40bb1090e36869ba7
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-08 19:28:13 -0800

    Update 2.tracking.data.R

--- 2.tracking.data.R
+++ 2.tracking.data.R
@@ -8,17 +8,17 @@
 repeated_stamps <- rep(time_stamps, park_capacity)
 
 # creating data frame with the time stamps sorted in ascending order
-long_visitor_tracker <- as.data.frame(matrix(sort(repeated_stamps)))
-colnames(long_visitor_tracker) <- c("time stamp")
+visitor_tracker <- as.data.frame(matrix(sort(repeated_stamps)))
+colnames(visitor_tracker) <- c("time stamp")
 
 # adding visitor column to data frame
-long_visitor_tracker$visitors <- rep(visitors)
+visitor_tracker$visitors <- rep(visitors)
 
-# === 2) first time stamp tracker ==============================================
-# for this section to work properly time stamp must be 1
-long_visitor_tracker$ride <- assigned_ride
+# adding ride column to data frame
+visitor_tracker$ride <- f.ride_picker(visitor_col, assigned_ride)
 
-# === 3) ..... =================================================================
+# adding spot in line column to the data frame
+visitor_tracker$spot_in_line <- f.get_spot_in_line(visitor_tracker)
 
 
 #___ end _______________________________________________________________________
