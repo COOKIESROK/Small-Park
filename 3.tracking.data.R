be8e8d486cc935466e0e33e12d7c6d7a24f9e20d
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-09 14:11:03 -0800

    Update 3.tracking.data.R

--- 3.tracking.data.R
+++ 3.tracking.data.R
@@ -3,9 +3,9 @@
 ## brief description what the script file is about
 #
 
-# === 1) visitor tracker =======================================================
+# === 1) visitor tracker ts 1===================================================
 # creating a list of time stamps that repeats each times tamp for each guest
-repeated_stamps <- rep(time_stamps, park_capacity)
+repeated_stamps <- rep(time_stamps[1], park_capacity)
 
 # creating data frame with the time stamps sorted in ascending order
 visitor_tracker <- as.data.frame(matrix(sort(repeated_stamps)))
@@ -27,6 +27,22 @@ visitor_tracker$spot_in_line <- f.get_spot_in_line_looped(visitor_tracker)
 # visitor_tracker$status <- NA
 # visitor_tracker$status <- f.get_status(visitor_tracker)
 visitor_tracker$status <- f.get_status_looped(visitor_tracker)
+
+# === 2) visitor tracker ts 2===================================================
+# copying visitor tracker
+visitor_tracker2 <-data.frame(visitor_tracker)
+
+# change the time stamps
+repeated_stamps2 <- rep(time_stamps[2], park_capacity)
+visitor_tracker2[1] <- repeated_stamps2
+
+# assign new rides to the visitors that went on a ride in the last time step
+
+
+# move waiting visitors one spot up in line
+f.move_up(visitor_tracker2)
+
+
 
 
 #___ end _______________________________________________________________________
