917de72e17e92777d76e2db8dcce856c438bd6b4
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-08 20:24:52 -0800

    Update 2.functions.R

--- 2.functions.R
+++ 2.functions.R
@@ -15,7 +15,7 @@ f.ride_picker <- function(visitor_col,assigned_ride){
   colnames(random_ride) <- "ride"
   assigned_ride <- random_ride
 }
-# added to visitor tracker in file 2
+# added to visitor tracker in file 3
 f.ride_picker(visitor_col, assigned_ride)
 
 ### I will start by working with random but eventually switch to biased rides
@@ -37,8 +37,6 @@ f.ride_picker(visitor_col, assigned_ride)
 # === 2) spot in line ==========================================================
 # this is so that it is possible for ALL visitors to be in line at the same ride
 spots <- visitors
-# creating spot in line column
-visitor_tracker$spot_in_line <- NA
 
 # creating a function that takes in visitor tracker and returns each visitor's 
 # spot in line
@@ -63,24 +61,35 @@ f.get_spot_in_line <- function(visitor_tracker){
     sort(rep(spots, ride_capacity))[1:lamest_visitors]
 }
 
+# === 2) status assigner =======================================================
+# creating states for visitors, they can either be riding or waiting to ride
+# any of the rides
+states <- c("r_coolest", "w_coolest", "r_okayest", "w_okayest", "r_lamest", "w_lamest")
 
-# # === 2) status assigner =======================================================
-# # creating states for visitors, they can either be riding or waiting to ride
-# # any of the rides
-# states <- c("r_coolest", "w_coolest", "r_okayest", "w_okayest", "r_lamest", "w_lamest")
-# 
-# # adding status column to visitor_tracker
-# visitor_tracker$status <- NA
-# 
-# # ride_capacity visitors get status riding for each ride
-# 
-# visitor_tracker[(visitor_tracker$ride == "coolest") & 
-#                   (visitor_tracker$spot_in_line <= 1), ]$spot_in_line <- 
-#   sort(rep(spots, ride_capacity))[1:coolest_visitors]
+# ride_capacity visitors get status riding for each ride, the rest get waiting
 
-
-
-
-
-
+# a function that takes in visitor_tracker and returns status of each visitor
+f.get_status <- function(visitor_tracker){
+  # for the visitors riding the coolest ride
+  visitor_tracker[(visitor_tracker$ride == "coolest") &
+                    (visitor_tracker$spot_in_line <= 1), ]$status <- states[1]
+  # for the visitors waiting to ride the coolest ride
+  if(coolest_visitors > ride_capacity){
+    visitor_tracker[(visitor_tracker$ride == "coolest") &
+                      (visitor_tracker$spot_in_line >= 2), ]$status <- states[2]}
+  # for the visitors riding the okayest ride
+  visitor_tracker[(visitor_tracker$ride == "okayest") &
+                    (visitor_tracker$spot_in_line <= 1), ]$status <- states[3]
+  # for the visitors waiting to ride the okayest ride
+  if(okayest_visitors > ride_capacity){
+    visitor_tracker[(visitor_tracker$ride == "okayest") &
+                      (visitor_tracker$spot_in_line >= 2), ]$status <- states[4]}
+  # for the visitors riding the lamest ride
+  visitor_tracker[(visitor_tracker$ride == "lamest") &
+                    (visitor_tracker$spot_in_line <= 1), ]$status <- states[5]
+  # for the visitors waiting to ride the lamest ride
+  if(lamest_visitors > ride_capacity){
+    visitor_tracker[(visitor_tracker$ride == "lamest") &
+                      (visitor_tracker$spot_in_line >= 2), ]$status <- states[6]}
+}
 
