7a0d93e599a3351f976a19d2199f5fc1026ec6e8
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-09 16:42:19 -0800

    Update 5.figures.R

--- 5.figures.R
+++ 5.figures.R
@@ -7,7 +7,7 @@
 # === 1) visitor distribution time step 1 ======================================
 # bar plot of visitor distribution at time step 1
 visitor_dist_timestep_1 <- table(visitor_tracker$ride[1:park_capacity,])
-barplot(visitor_dist_timestamp_1, ylab = "Number of visitors", 
+barplot(visitor_dist_timestep_1, ylab = "Number of visitors", 
         xlab = "Ride", main = "Visitor distribution time step 1")
 
 # === 2) visitor distribution time step 2 ======================================
