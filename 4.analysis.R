bb7fe8892f951897aa703da9397832d5517abb0b
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-14 09:14:29 -0800

    Update 4.analysis.R

--- 4.analysis.R
+++ 4.analysis.R
@@ -3,20 +3,33 @@
 ## brief description what the script file is about
 #
 
-## if you have clear chunks of code, use collapsible dividers (# ==== or # ----)
-## and number the sections. Provide brief description for each section here
-## # ***** or #_____ does not make sections 
-
 # ******************************************************************************
 ## use descriptive names 
 # 1) read clean data  load data from clean data folder and ...
 # 2) anova      lm to test ...
 
-# === 1) read clean data =======================================================
+# === 1) read data =============================================================
+# reading data
+visitor_data <- read.csv(paste(p.data, "/", "CompleteVisitorTracker.csv", sep = ""))
 
+#inspecting data
+head(visitor_data)
+str(visitor_data)
 
+# === analyzing visitor satisfaction ===============
+# looking at the least and most satisfied visitors
+satisfaction_range <- range(visitor_data$satisfaction)
+least_satisfied_visitor <- range(visitor_data$satisfaction)[1]
+least_satisfied_visitor_id <- 
+  visitor_data[visitor_data$satisfaction == least_satisfied_visitor,][3]
+most_satisfied_visitor <- range(visitor_data$satisfaction)[2]
+most_satisfied_visitor_id <- 
+  visitor_data[visitor_data$satisfaction == most_satisfied_visitor,][3]
 
-# === 2) anova ===========================================================
+# looking at mean satisfaction
+mean_satisfaction <- mean(new_visitor_tracker$satisfaction)
+
+# satisfaction distribution histogram?
 
 
 
