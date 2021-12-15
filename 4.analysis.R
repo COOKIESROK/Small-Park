f4a71493bdb35c75269492c58db78e96514853b8
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-14 17:03:14 -0800

    Update 4.analysis.R

--- 4.analysis.R
+++ 4.analysis.R
@@ -1,12 +1,15 @@
 
 # === description ===========================================================================
-## brief description what the script file is about
-#
+# This file looks at the descriptive statistics of the data file 
+# "CompleteVisitorTracker.csv" generated in 3.tracking.data.R
+# This script would be much more useful if used to compare cycles with different
+# parameters (maybe in which rides were randomly assigned vs one in which they
+# they were selected based on a factor such as current wait time or amount of 
+# cool points they provide).
 
 # ******************************************************************************
-## use descriptive names 
-# 1) read clean data  load data from clean data folder and ...
-# 2) anova      lm to test ...
+# 1) read data
+# 2) descriptive stats visitor satisfaction
 
 # === 1) read data =============================================================
 # reading data
@@ -16,7 +19,7 @@ visitor_data <- read.csv(paste(p.data, "/", "CompleteVisitorTracker.csv", sep = 
 head(visitor_data)
 str(visitor_data)
 
-# === analyzing visitor satisfaction ===============
+# === 2) descriptive stats visitor satisfaction ================================
 # looking at the least and most satisfied visitors
 satisfaction_range <- range(visitor_data$satisfaction)
 least_satisfied_visitor <- range(visitor_data$satisfaction)[1]
@@ -26,12 +29,19 @@ most_satisfied_visitor <- range(visitor_data$satisfaction)[2]
 most_satisfied_visitor_id <- 
   visitor_data[visitor_data$satisfaction == most_satisfied_visitor,][3]
 
-# looking at mean satisfaction
-mean_satisfaction <- mean(new_visitor_tracker$satisfaction)
+# looking at mean satisfaction for all visitors throughout the day
+mean_satisfaction <- mean(visitor_data$satisfaction)
 
-# satisfaction distribution histogram?
+# looking at mean satisfaction for all visitors at the end of the day
+final_mean_satisfaction <- 
+  mean(visitor_data$satisfaction[visitor_data$time_step == 10])
 
+# looking at standard deviation of satisfaction
+var_satisfaction <- var(visitor_data$satisfaction)
 
+# looking at standard deviation of satisfaction at the end of the day
+final_var_satisfaction <- 
+  var(visitor_data$satisfaction[visitor_data$time_step == 10])
 
 #___ end _______________________________________________________________________
 
