c5654f1b3af8f91afa7f67a4fa18281adf2d6c39
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2022-08-13 13:08:06 -0500

    Update 1.main2.0.R

--- 1.main2.0.R
+++ 1.main2.0.R
@@ -4,7 +4,7 @@
 # author: Sofia Graves
 # R version
 # "R version 4.1.2 (2021-11-01)"
-# NOTE: run the 1.main.R before starting your session.
+# NOTE: run the 1.main.R file before starting your session.
 
 # === script index and note ====================================================
 # 1.main.R        
@@ -18,23 +18,72 @@
 # should be run fully before moving on to script 2.functions.R
 
 
-# === global variables =========================================================
-
-wk.dir <- getwd()
+# === user input ====
 # max number of visitors
-park_capacity <- 12
-visitors <- c(1:park_capacity)
+park_capacity <- 15
+
+# rides data frame
+# ride_name column
+rides <- as.data.frame(c("coolest", "okayest", "lamest", "newest"))
+#changing name of column
+colnames(rides) <- "ride_name"
+
 # max number of visitors that can ride a ride at each time
-ride_capacity <- 3
-# the 3 rides in the park
-rides <- c("coolest", "okayest", "lamest")
+# the first number corresponds to the capacity of the coolest ride (see rides)
+# adding to rides data frame
+rides$ride_capacity <- c(2, 2, 2, 2)
+
+
+# probability a rider will choose a given ride
+# the first number corresponds to the probability of the coolest ride (see rides)
+# adding to rides data frame
+rides$ride_prob <- c(0.8, 0.4, 0.2, 0.8)
+
 # the number of time steps in a day
-cycle <- 5
+cycle <- 12
+
+# cool points that each ride gives a visitor (in the same order as the rides - 
+# coolest first)
+each_ride_points <- c(10, 5, 1, 12)
+
+# number of times each strategy will run
+run_for <- 10
+
+# === global variables =========================================================
+# getting working directory
+wk.dir <- getwd()
+
+# counting visitors
+visitors <- c(1:park_capacity)
+
+# creating states for visitors, they can either be riding or waiting to ride
+# any of the rides
+# this vector will be replaced with the loop I just created 
+# states <- c("r_coolest", "w_coolest", "r_okayest", "w_okayest", "r_lamest", 
+#             "w_lamest")
+states <- vector()
+# filling in the states vector
+for(i in 1:length(rides)) {
+  new_states <- c(paste("r_", rides$ride_name[i], sep = ""), 
+                  paste("w_", rides$ride_name[i], sep = ""))     
+  states <- c(states, new_states) 
+}
+
+# counting time steps
 time_steps <- c(1:cycle)
-# cool points that each ride gives a visitor (coolest, okayest, lamest)
-cool_points <- c(10,5,1)
+
+# loop for cool points
+cool_points <- vector()
+for(i in 1:length(each_ride_points)){
+  add_points <- c(each_ride_points[i], 0)
+  cool_points <- c(cool_points, add_points)
+}
+
 # true max satisfaction would come from riding the coolest ride every time step
 true_max_satisfaction <- cool_points[1]*cycle
+
+# counting runs
+runs <- c(1:run_for)
 
 # === folder management ========================================================
 # folder names
@@ -51,5 +100,4 @@ for(i in 1:length(folder.names)){
 p.data <- paste(wk.dir, "/", folder.names[1], "/", sep = "")
 p.fig <- paste(wk.dir, "/", folder.names[2], "/", sep = "")
 
-#___ end _______________________________________________________________________
-
+#___ end of 1.main _____________________________________________________________
